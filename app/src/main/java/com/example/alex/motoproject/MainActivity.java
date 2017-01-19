d5b633e98ff2819521879ee8a323f99d24c35638
IhorPakharenko <ihorpakharenko@gmail.com>
2017-01-19 23:04:21 +0200

    Add map

--- app/src/main/java/com/example/alex/motoproject/MainActivity.java
+++ app/src/main/java/com/example/alex/motoproject/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.alex.motoproject;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -66,12 +68,14 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // Handle navigation view item clicks here.
+        FragmentManager mFragmentManager = getSupportFragmentManager();
+        FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
         int id = item.getItemId();
-
         switch (id){
             case R.id.nav_maps:
-                //TODO fragment maps
+                MapFragment mMapFragment = new MapFragment();
+                mFragmentTransaction.replace(R.id.container, mMapFragment);
+                mFragmentTransaction.commit();
                 break;
             case R.id.nav_friends:
                 //TODO fragment friends list
---
+++ app/src/main/java/com/example/alex/motoproject/MapFragment.java
@@ -0,0 +1,85 @@
+package com.example.alex.motoproject;
+
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+/**
+ * The fragment that contains a map from Google Play Services.
+ */
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    private MapView mMapView;
+    private GoogleMap mMap;
+
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        mMapView = (MapView) view.findViewById(R.id.map);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.onResume();
+        mMapView.getMapAsync(this);//when you already implement OnMapReadyCallback in your fragment
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        //TODO: check if this line of code is needed at all
+        mMap = map;
+        
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        //TODO: fix the error
+        if (checkPermissionLocation()) {
+            map.setMyLocationEnabled(true);
+        }
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+    }
+
+    //Helper method to check if the app is allowed to get location
+    private boolean checkPermissionLocation()  {
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            // Show rationale and request permission.
+            return false;
+        }
+    }
+}
