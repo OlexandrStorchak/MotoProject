9310341939c09537b6dbb1fde47796bf097c218a
Alex <new15@ukr.net>
2017-02-11 22:45:17 +0200

    Some refactor and tune

--- app/src/main/java/com/example/alex/motoproject/mainActivity/MainActivityPresenter.java
+++ app/src/main/java/com/example/alex/motoproject/mainActivity/MainActivityPresenter.java
@@ -3,7 +3,7 @@ package com.example.alex.motoproject.mainActivity;
 
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivityPresenter implements PresenterInterface {
+public class MainActivityPresenter implements MainPresenterInterface {
     private MainViewInterface mainView;
 
 
--- app/src/main/java/com/example/alex/motoproject/mainActivity/MainPresenterInterface.java
+++ app/src/main/java/com/example/alex/motoproject/mainActivity/MainPresenterInterface.java
@@ -3,7 +3,7 @@ package com.example.alex.motoproject.mainActivity;
 
 import com.google.firebase.auth.FirebaseUser;
 
-interface PresenterInterface {
+interface MainPresenterInterface {
 
     void onLogin(FirebaseUser user);
 
